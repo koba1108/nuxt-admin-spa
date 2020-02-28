ecc9da6164c9e75de8de274844bd79281266dfa0
koba1108 <paseli.koba@gmail.com>
2020-02-28 18:21:44 +0900

    add

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <header>ログイン画面だよ</header>
+    <main>
+      <input v-model="email" type="email">
+      <input v-model="password" type="password">
+      <the-button type="login" @click="doLogin">ログイン</the-button>
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        email: '',
+        password: '',
+      }
+    },
+    methods: {
+      async doLogin() {
+        try {
+          await this.$auth().signInWithEmailAndPassword(this.email, this.password)
+          this.$router.push('/dashboard')
+        } catch (e) {
+          alert(e.message)
+          console.error(e)
+        }
+      },
+    },
+  }
+</script>
