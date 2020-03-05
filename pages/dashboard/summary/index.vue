586bc9f343a5aca31690d23e2470084c3be14d86
koba1108 <paseli.koba@gmail.com>
2020-03-05 17:27:51 +0900

    add from to fields

--- pages/dashboard/summary/index.vue
+++ pages/dashboard/summary/index.vue
@@ -3,69 +3,123 @@
     <v-col cols="12">
       <v-card>
         <v-card-title>TOP5</v-card-title>
-        <v-list disabled>
-          <v-list-item-group v-model="top5" color="primary">
-            <v-list-item v-for="(t, i) in top5" :key="i">
-              <v-list-item-content>
-                <v-list-item-title>
-                  {{ t.key }}: {{ t.text }}
-                </v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
+        <v-simple-table :fixed-header="true">
+          <tbody>
+          <tr v-for="(t) in top5" :key="t.key">
+            <td>{{ t.key }}</td>
+            <td>{{ t.text }}</td>
+          </tr>
+          </tbody>
+        </v-simple-table>
       </v-card>
     </v-col>
-    <v-col cols="12">
+
+    <v-col v-for="g in graphs" :key="g.id" cols="12">
       <v-card>
-        <v-card-title>グラフその1</v-card-title>
-        <v-card-text class="h-500px">
-          <line-chart v-if="chartData" :chart-data="chartData" :options="options"/>
+        <v-card-title>
+          <v-row>
+            <v-col cols="4">
+              <v-card-title>{{ g.name }}</v-card-title>
+            </v-col>
+            <v-col cols="4">
+              <v-text-field
+                v-model="g.selected.from"
+                type="date"
+                label="From"
+                prepend-icon="event"
+                @click="openDatepicker('from', g)"
+              />
+            </v-col>
+            <v-col cols="4">
+              <v-text-field
+                v-model="g.selected.to"
+                type="date"
+                label="To"
+                prepend-icon="event"
+                @click="openDatepicker('to', g)"
+              />
+            </v-col>
+          </v-row>
+        </v-card-title>
+        <v-card-text v-if="g.chartData" class="h-500px">
+          <line-chart :chart-data="g.chartData" :options="chartOptions"/>
         </v-card-text>
       </v-card>
     </v-col>
-    <v-col cols="12">
-      <v-card>
-        <v-card-title>グラフその2</v-card-title>
-        <v-card-text class="h-500px">
-          <line-chart v-if="chartData2" :chart-data="chartData" :options="options"/>
-        </v-card-text>
-      </v-card>
-    </v-col>
+    <v-dialog :max-width="dialog.width" v-model="dialog.active">
+      <v-date-picker
+        show-current
+        no-title
+        reactive
+        @click:date="setPickedDate"
+      />
+    </v-dialog>
   </v-row>
 </template>
 
 <script>
-  const range = (min, max) => {
-    return Array.from(Array(max + 1).keys()).filter(n => n >= min)
-  }
-  const randInt = () => {
-    return Math.floor(Math.random() * (50 - 5 + 1)) + 5
-  }
-  const makeTestData = () => {
-    return range(1, 12).map(i => randInt())
-  }
+  const range = (min, max) => Array.from(Array(max + 1).keys()).filter(n => n >= min)
+  const randInt = () => Math.floor(Math.random() * (50 - 5 + 1)) + 5
+  const makeTestData = () => range(1, 12).map(i => randInt())
+
+  const TOP5 = [
+    { key: 'driver_key1', text: '10000km' },
+    { key: 'driver_key2', text: '1000km' },
+    { key: 'driver_key3', text: '100km' },
+    { key: 'driver_key4', text: '10km' },
+    { key: 'driver_key5', text: '1km' },
+  ]
+
+  const GRAPHS = [
+    {
+      id: 'graph_1',
+      name: 'グラグその1',
+      chartData: null,
+      selected: {
+        from: null,
+        to: null,
+      },
+    },
+    {
+      id: 'graph_2',
+      name: 'グラグその2',
+      chartData: null,
+      selected: {
+        from: null,
+        to: null,
+      },
+    },
+  ]
+
+  const TYPE_FROM = 'from'
+  const TYPE_TO = 'to'
+  const TARGET_TYPES = [TYPE_FROM, TYPE_TO]
 
   export default {
     data() {
       return {
-        top5: [
-          { key: 'driver_key1', text: '10000km', },
-          { key: 'driver_key2', text: '1000km', },
-          { key: 'driver_key3', text: '100km', },
-          { key: 'driver_key4', text: '10km', },
-          { key: 'driver_key5', text: '1km', },
-        ],
-        chartData: null,
-        chartData2: null,
-        options: {
-          responsive: true,
-          maintainAspectRatio: false,
+        top5: TOP5, // todo: fetch from API
+        graphs: GRAPHS,
+        picker: {
+          type: null, // TYPE_FROM or TYPE_TO
+          graphId: null,
+        },
+        dialog: {
+          active: false,
+          width: 290,
         },
       }
     },
+    computed: {
+      chartOptions() {
+        return {
+          responsive: true,
+          maintainAspectRatio: false,
+        }
+      },
+    },
     methods: {
-      getData() {
+      fetchData() {
         return {
           labels: range(1, 12).map(i => `${i}月`),
           datasets: [
@@ -90,10 +144,24 @@
           ],
         }
       },
+      openDatepicker(selectedType, graph) {
+        if(TARGET_TYPES.includes(selectedType)) {
+          this.picker.graphId = graph.id
+          this.picker.type = selectedType
+          this.dialog.active = true
+        }
+      },
+      setPickedDate(pickedDate) {
+        if(TARGET_TYPES.includes(this.picker.type)) {
+          const currentGraph = this.graphs.find(g => g.id === this.picker.graphId)
+          currentGraph.selected[this.picker.type] = pickedDate
+          currentGraph.chartData = this.fetchData()
+        }
+        this.dialog.active = false
+      },
     },
     mounted() {
-      this.chartData = this.getData()
-      this.chartData2 = this.getData()
+      this.graphs.map(g => g.chartData = this.fetchData())
     },
   }
 </script>
