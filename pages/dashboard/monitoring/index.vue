a3e6ee274e465202bd845d83a22811abf5d4c7ca
koba1108 <paseli.koba@gmail.com>
2020-03-27 17:54:29 +0900

    last...

--- pages/dashboard/monitoring/index.vue
+++ pages/dashboard/monitoring/index.vue
@@ -147,36 +147,80 @@
 
   const mapStyles = [
     {
+      featureType: 'administrative',
+      elementType: 'geometry',
+      stylers: [
+        {
+          visibility: 'off',
+        },
+      ],
+    },
+    {
       featureType: 'administrative.land_parcel',
       stylers: [
-        { visibility: 'off' },
+        {
+          visibility: 'off',
+        },
       ],
     },
     {
       featureType: 'administrative.neighborhood',
       stylers: [
-        { visibility: 'off' },
+        {
+          visibility: 'off',
+        },
+      ],
+    },
+    {
+      featureType: 'poi',
+      stylers: [
+        {
+          visibility: 'off',
+        },
       ],
     },
     {
       featureType: 'poi',
       elementType: 'labels.text',
       stylers: [
-        { visibility: 'off' },
+        {
+          visibility: 'off',
+        },
       ],
     },
     {
       featureType: 'road',
       elementType: 'labels',
       stylers: [
-        { visibility: 'off' },
+        {
+          visibility: 'off',
+        },
+      ],
+    },
+    {
+      featureType: 'road',
+      elementType: 'labels.icon',
+      stylers: [
+        {
+          visibility: 'off',
+        },
+      ],
+    },
+    {
+      featureType: 'transit',
+      stylers: [
+        {
+          visibility: 'off',
+        },
       ],
     },
     {
       featureType: 'water',
       elementType: 'labels.text',
       stylers: [
-        { visibility: 'off' },
+        {
+          visibility: 'off',
+        },
       ],
     },
   ]
